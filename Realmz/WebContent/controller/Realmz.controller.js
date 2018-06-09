f68517ed47e8cfb9960bbc2ac6060f896ec02af0
rocka <rocka@rocka-MS-7816>
2018-06-09 17:56:56 +0200

    Added kingdom controller logic.

--- Realmz/WebContent/controller/Realmz.controller.js
+++ Realmz/WebContent/controller/Realmz.controller.js
@@ -1,16 +1,20 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
    "sap/m/MessageToast",
-   "realmz/Player"
-], function (Controller,MessageToast, Player) {
+   "realmz/Player",
+   "realmz/Kingdom"
+], function (Controller,MessageToast, Player, Kingdom) {
    "use strict";
-   var player, world;
+   var player, kingdom, world;
    
    return Controller.extend("realmz.ui5.controller.Realmz",
    {
 	   onInit : function() {
 	     player = new Player();
+	     kingdom = new Kingdom();
 	     player.generatePlayerName();
+	     kingdom.generateName();
+	     kingdom.setPlayer(player);
 ;	     var date = new Date ();
 	   },
 	   
@@ -23,11 +27,7 @@ sap.ui.define([
 	     setTimeout(MessageToast.show("Ruler "+player.getFirstName()+" "+player.getLastName()+" is now Ruling!"), 3000);
 		 
 	   },
-     onShowHello : function () {
-       MessageToast.show("Hello World");
-     },
-     
-     
+  
      wakeup: function()
      {
        
