6d576a02961c56b82470fd537ffcd69e98f19620
rocka <rocka@rocka-MS-7816>
2018-06-13 20:54:15 +0200

    Added event class

--- Realmz/WebContent/class/Event.js
+++ Realmz/WebContent/class/Event.js
@@ -4,38 +4,12 @@ sap.ui.define([ "sap/ui/base/ManagedObject" ], function( ManagedObject )
   {
     metadata: {
       properties: {
-        firstName:  { type: "string", defaultValue: "Unknown Player" },
-        lastName:   { type:"string", defaultValue: ""},
-        gender: {type:"boolean", defaultValue: false},
-        score:      { type: "int",    defaultValue: 0       },
-        world:      { type: "realmz.World" }
       }
     },
     
-    generatePlayerName: function(gender) {
-      var firstNameMale = ["Hagen", "Siegfried", "Leonardo", "Josef", "Pablo", "Kratos", "Harisson", "Mahatama", "Sun", "Kim", "Donald", "Vladimir","George","Özkan","Merlin","Daniel","Volker","Ernst","Adolf"];
-      var firstNameFemale = ["Larissa", "Morgaine", "Elaine", "Katharina", "Elizabeth","Hannah", "Anna", "Leah","Donna","Bonnie"];
-      var lastName = ["Forrester", "Stark", "Liedtke", "Bolton", "Holden","Trussler","Tune"];
-      var gender = Math.random() >= 0.5;
-      var name = lastName[Math.floor(Math.random()*lastName.length)];
-      this.setLastName(name);
-      if (gender == true) {
-        var firstName = firstNameMale[Math.floor(Math.random()*firstNameMale.length)];
-        this.setFirstName(firstName);
+    handleEvent: function(kingdom) {
+        
       }
-      
-      if (gender == false) {
-        var firstName = firstNameFemale[Math.floor(Math.random()*firstNameFemale.length)];
-        this.setFirstName(firstName);
-      }
-      
-    },
-    
-    damage: function( amount )
-    {
-      this.setHealth( this.getHealth() - amount );
-    }
-    
   });
   
   
