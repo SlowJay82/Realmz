aa6a3ccf79e8ec1b8d0e18f43039f835c147d6db
rocka <rocka@rocka-MS-7816>
2018-06-13 19:16:58 +0200

    Added turn logic

--- Realmz/WebContent/class/Kingdom.js
+++ Realmz/WebContent/class/Kingdom.js
@@ -9,12 +9,12 @@ sap.ui.define([ "sap/ui/base/ManagedObject" ], function( ManagedObject )
         population:   { type:"int", defaultValue: 1000},
         food:      { type: "int",    defaultValue: 1000       },
         gold:      { type: "int" , defaultValue: 1000},
+        // Adjustable jobs
         peasant: {type: "int", defaultValue: 500},
         soldier: {type: "int", defaultValue: 100},
         scientist: {type: "int", defaultValue: 150},
         merchant: {type: "int", defaultValue: 150},
         intelligence: {type: "int", defaultValue: 100}
-        
       }
     },
     
--- Realmz/WebContent/controller/Realmz.controller.js
+++ Realmz/WebContent/controller/Realmz.controller.js
@@ -9,23 +9,31 @@ sap.ui.define([
    
    return Controller.extend("realmz.ui5.controller.Realmz",
    {
+     
 	   onInit : function() {
 	     player = new Player();
 	     kingdom = new Kingdom();
 	     player.generatePlayerName();
 	     kingdom.generateName();
 	     kingdom.setPlayer(player);
-;	     var date = new Date ();
+	     
 	   },
 	   
 	   onNextTurn : function() {
+	     // Init Date
+	     if (!date) {
+	       var date = new Date('1000-01-01T00:00:00');
+	     }; 
+	     // Set day
+	     this.byId("date").setText(date);
+	     // Output
 	     MessageToast.show("A new day has begun!"); 
 	   },
 	   
 	   onAfterRendering : function() {
 	     MessageToast.show("Welcome to Realmz my Lord"); 
 	     setTimeout(MessageToast.show("Ruler "+player.getFirstName()+" "+player.getLastName()+" is now Ruling!"), 3000);
-		 
+	     
 	   },
   
      wakeup: function()
