a7ae6232ce294449f2a89905cca9793fd89fedd2
rocka <rocka@rocka-MS-7816>
2018-07-09 21:54:21 +0200

    Added event functions

--- WebContent/class/Event.js
+++ WebContent/class/Event.js
@@ -1,20 +1,36 @@
-sap.ui.define([ "sap/ui/base/ManagedObject",
-  "./Random"], function( ManagedObject, Random )
-{
-  return ManagedObject.extend( "realmz.Event",
-  {
-    metadata: {
-      properties: {
+sap.ui.define([ "sap/ui/base/ManagedObject", "./Random" ], function(
+    ManagedObject, Random) {
+  return ManagedObject.extend("realmz.Event", {
+    metadata : {
+      properties : {}
+    },
+
+    // Random Events
+    randomEvents : function(kingdom) {
+      this.randomEventPlague(kingdom);
+      // Barbarian
+    },
+    // Kingdom dependant Events
+    kingdomEvents : function() {
+      // Civil Riot
+
+      // Espionage
+    },
+
+    randomEventPlague : function(kingdom) {
+      
+      var plagues = [ "frog", "lice", "flea" ];
+      var plague = "";
+      var rnd = new Random();
+      var res = rnd.nextInt(0, 100);
+      if (res > 90) {
+        kingdom.setGold(100);
+        // alert("Plague");
       }
     },
-    
-    sample: function()
-    {
-     
-      }
-     
+    randomEventDisease : function() {
+      var diseases = [];
     }
-  );
-  
-  
+  });
+
 });
