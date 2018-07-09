efc98dff55162e3aebfd5eeca5bda15a260b5563
rocka <rocka@rocka-MS-7816>
2018-07-09 22:09:23 +0200

    Added defines

--- WebContent/class/Event.js
+++ WebContent/class/Event.js
@@ -1,5 +1,5 @@
-sap.ui.define([ "sap/ui/base/ManagedObject", "./Random" ], function(
-    ManagedObject, Random) {
+sap.ui.define([ "sap/ui/base/ManagedObject", "./Random", "sap/m/MessageBox" ], function(
+    ManagedObject, Random, MessageBox) {
   return ManagedObject.extend("realmz.Event", {
     metadata : {
       properties : {}
@@ -23,6 +23,7 @@ sap.ui.define([ "sap/ui/base/ManagedObject", "./Random" ], function(
       var plague = "";
       var rnd = new Random();
       var res = rnd.nextInt(0, 100);
+      //MessageBox.confirm("Test");
       if (res > 90) {
         kingdom.setGold(100);
         // alert("Plague");
